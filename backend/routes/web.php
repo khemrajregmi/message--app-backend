3d874d9be2d19f92fe04e5b76112c3bddc76f353
Khem Raj Regmi <khemrr067@gmail.com>
2025-02-22 16:00:36 +0100

    added backend and frontend files

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MessageController;
 
-Route::get('/', function () {
-    return view('welcome');
+oute::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('messages', MessageController::class);
 });
